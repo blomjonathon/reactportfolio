d36eb1ee856303aa5ace25fb806724455cc1b32b
Jonathon Blom <blomjonathon@gmail.com>
2024-03-06 16:57:07 -0600

    made footer component, got images to load on section component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../App.css";
+
+export default function Footer() {
+  return (
+    <>
+     <section class="contact">
+            <h4 id="contact">Contact</h4>
+            <div>
+                images
+            </div>
+        </section>
+        <footer>© Copyright 2023 | Created by Jonathon Blom </footer>
+    </>
+  );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header"
 import Section from "../components/Section";
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <>
       <Header/>
       <Section />
+      <Footer />
     </>
   );
 }
