86557997bee6b30dd8a3614b3bab691b4908cf8e
Jonathon Blom <blomjonathon@gmail.com>
2024-03-06 13:55:21 -0600

    cut down boiler plate for starting point

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,17 @@
 import { useEffect } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Cookie from "js-cookie"
-import AppProvider from "./providers/AppProvider"
-import Header from "./components/Header"
-import { Home, Note, Auth } from "./pages"
+import Home from "./pages/Home"
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 export default function App() {
 
-  function verifyUser(){
-    const cookie = Cookie.get("auth_cookie")
-  }
-
-  useEffect(() => {
-    verifyUser()
-  },[])
-
   return (
-    <AppProvider>
-      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/note/:id" element={<Note />} />
-          <Route path="*" element={<Note />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
-    </AppProvider>
   )
 }
 
