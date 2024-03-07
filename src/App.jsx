c5f1322ef63cb9989a7b74522d925a954845c02c
Jonathon Blom <blomjonathon@gmail.com>
2024-03-07 11:58:56 -0600

    made nav work better with highlighted links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import Project from "./components/Project";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import "./App.css";
@@ -10,21 +10,21 @@ export default function App() {
   return (
     <BrowserRouter>
       <nav>
-  <ul>
-    <li>
-      <NavLink to="/about" activeStyle={{ color: "red", fontWeight: "bold" }}>About Me</NavLink>
-    </li>
-    <li>
-      <NavLink to="/portfolio" activeStyle={{ color: "red", fontWeight: "bold" }}>Portfolio</NavLink>
-    </li>
-    <li>
-      <NavLink to="/contact" activeStyle={{ color: "red", fontWeight: "bold" }}>Contact</NavLink>
-    </li>
-    <li>
-      <NavLink to="/resume" activeStyle={{ color: "red", fontWeight: "bold" }}>Resume</NavLink>
-    </li>
-  </ul>
-</nav>
+        <ul>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/resume">Resume</NavLink>
+          </li>
+        </ul>
+      </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
