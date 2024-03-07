57336c055cf1ce0753e747d600369bb2e9be83be
Jonathon Blom <blomjonathon@gmail.com>
2024-03-06 22:40:28 -0600

    trying to deploy to netlify

--- server/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router()
-const noteRoutes = require("./note.routes");
-const userRoutes = require("./user.routes");
-
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require("cookie-parser");
-
-const db = require('./config/connection');
-
-const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
